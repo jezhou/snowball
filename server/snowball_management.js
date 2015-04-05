2b9a3661d1b51cc105fd05153768c8af1c5aaece
jezhou <jesse.cy.zhou@gmail.com>
2015-04-05 12:02:04 -0700

    checkpoint

--- server/snowball_management.js
+++ server/snowball_management.js
@@ -1,6 +1,7 @@
 Meteor.startup(function(){
   Meteor.setInterval(function(){
 
+    SnowballCollection.update({}, {$inc:{score: 1}}, {multi: true});
     SnowballCollection.update({}, {$inc:{velocity: -1}}, {multi: true});
     SnowballCollection.remove({velocity: {$lte: 0}});
 
